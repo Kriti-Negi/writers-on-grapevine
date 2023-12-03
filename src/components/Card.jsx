e860d533c4c4c54d18e86eafddd957f6c395dba3
Sanskriti Negi <sanskritinegi@Sanskritis-iMac.local>
2023-12-03 10:43:35 -0700

    sdfsd

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function Card(props){
     return (
         <div className="card">
-            <Link to = {"#/" + props.index} style = {{textDecoration: 'none', color: '#000'}}>
+            <Link to = {"/" + props.index} style = {{textDecoration: 'none', color: '#000'}}>
                 <h3>{props.cardTitle}</h3>
                 <p className="author">{props.author}</p>
                 <p className="short">{props.piece.substring(0, 400)}</p>
